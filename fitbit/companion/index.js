881934865de853feca98b4a5813ee3fea6317566
HaoChen <lihaochen987@gmail.com>
2023-04-07 11:46:51 +1200

    Refactor getDate into separate file

--- fitbit/companion/index.js
+++ fitbit/companion/index.js
@@ -4,6 +4,7 @@
 
 import * as messaging from "messaging";
 import {settingsStorage} from "settings";
+import getCurrentDateInNZST from "./getDate";
 
 // TODO: Make this an env variable
 const ENDPOINT = "***REMOVED***";
@@ -82,46 +83,4 @@ function restoreSettings() {
             fetchPatientData(data.user_id, data.access_token);
         }
     }
-}
-
-function isNZDST(date) {
-    const lastSundayOfSeptember = new Date(date.getFullYear(), 8, -0).getLastSunday();
-    const firstSundayOfApril = new Date(date.getFullYear(), 3, 1).getFirstSunday();
-
-    return date >= lastSundayOfSeptember && date < firstSundayOfApril;
-}
-
-Date.prototype.getLastSunday = function () {
-    const lastDayOfMonth = new Date(this.getFullYear(), this.getMonth() + 1, 0);
-    const day = lastDayOfMonth.getDay();
-    return new Date(lastDayOfMonth.setDate(lastDayOfMonth.getDate() - day));
-};
-
-Date.prototype.getFirstSunday = function () {
-    const firstDayOfMonth = new Date(this.getFullYear(), this.getMonth(), 1);
-    const day = firstDayOfMonth.getDay();
-    return new Date(firstDayOfMonth.setDate(firstDayOfMonth.getDate() + (7 - day) % 7));
-};
-
-function getCurrentDateInNZST() {
-    const now = new Date();
-
-    const nzOffset = 12 * 60;
-    const nzdtOffset = 13 * 60;
-
-    const isDaylightSaving = isNZDST(now);
-    const nzCurrentOffset = isDaylightSaving ? nzdtOffset : nzOffset;
-
-    const nzMilliseconds = now.getTime() + (nzCurrentOffset * 60 * 1000);
-    const nzDate = new Date(nzMilliseconds);
-
-    return nzDate.toLocaleString('en-NZ', {
-        timeZone: 'UTC',
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-    });
 }
