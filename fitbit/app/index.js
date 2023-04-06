3efab34061ea5f9108c26220bbfa68bc6a49d5c6
HaoChen <lihaochen987@gmail.com>
2023-04-06 12:19:40 +1200

    Companion sends event every 15 seconds

--- fitbit/app/index.js
+++ fitbit/app/index.js
@@ -7,16 +7,18 @@ clock.granularity = "minutes";
 
 // Get a handle on the <text> element
 const testText = document.getElementById("testText");
+let count = 0;
 
 // Message is received from companion
 messaging.peerSocket.onmessage = evt => {
     const totalUserSleep = evt.data.totalMinutesAsleep;
+    count += 1
     // Am I Tired?
     if (totalUserSleep) {
         // Had at least 5 hours sleep
         testText.text = `Awake! ${totalUserSleep}`;
     } else {
         // Had less than 5 hours sleep
-        testText.text = `Sleepy... ${totalUserSleep}`;
+        testText.text = `${count}`;
     }
 };
