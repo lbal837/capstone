1e47f8b0ed41ff2448c65844186c09f1a10e2d67
HaoChen <lihaochen987@gmail.com>
2023-04-18 17:15:29 +1200

    Refactor profileBox

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -24,45 +24,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ProfileBox extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      //inkwell should make card clickable
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => const MyDataPage(title: 'LifeSavers')),
-        );
-      },
-      child: Card(
-        child: Row(
-          children: [
-            Container(
-                padding: EdgeInsets.all(30.0),
-                child: const Icon(Icons.account_circle, size: 70),
-                decoration: BoxDecoration(
-                  color: Colors.deepPurple,
-                  borderRadius: BorderRadius.circular(15),
-                )),
-            Container(
-              padding: EdgeInsets.all(5.0),
-              alignment: Alignment.center,
-              child: Text("Patient Name/ Number"),
-            ),
-            Container(
-              padding: EdgeInsets.all(10.0),
-              alignment: Alignment.center,
-              child: Text("Status"),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 class MyDataPage extends StatefulWidget {
   const MyDataPage({super.key, required this.title});
   final String title;
