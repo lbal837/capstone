9f8e0b64acae436d2d818cf8e6884769c9d25625
HaoChen <lihaochen987@gmail.com>
2023-04-18 17:14:04 +1200

    Refactor homepage

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:frontend/ui/home/home.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -19,33 +20,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
       ),
       home: const MyHomePage(title: 'LifeSavers'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView(children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [ProfileBox(), ProfileBox(), ProfileBox(), ProfileBox()],
-        ),
-      ]),
     );
   }
 }
