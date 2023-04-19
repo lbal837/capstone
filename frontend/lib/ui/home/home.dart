5054a2b89619b3a553f65b4e22b19155328af3a6
HaoChen <lihaochen987@gmail.com>
2023-04-19 18:48:40 +1200

    Refactor out Signup, Confirm and Login

--- frontend/lib/ui/home/home.dart
+++ frontend/lib/ui/home/home.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/ui/confirmation/confirmation_screen.dart';
+import 'package:frontend/ui/home/widgets/home_confirm_user_button.dart';
+import 'package:frontend/ui/home/widgets/home_login_user_button.dart';
 import 'package:frontend/ui/home/widgets/home_profile_box.dart';
-import 'package:frontend/ui/login/login_screen.dart';
-import 'package:frontend/ui/signup/signup_screen.dart';
+import 'package:frontend/ui/home/widgets/home_sign_up_user_button.dart';
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
@@ -20,60 +20,9 @@ class _MyHomePageState extends State<MyHomePage> {
             const ProfileBox(),
             const ProfileBox(),
             const ProfileBox(),
-            Container(
-              padding:
-                  const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
-              width: screenSize.width,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SignUpScreen()),
-                  );
-                },
-                child: const Text(
-                  'Sign Up',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            Container(
-              padding:
-                  const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
-              width: screenSize.width,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ConfirmationScreen()),
-                  );
-                },
-                child: const Text(
-                  'Confirm Account',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            Container(
-              padding:
-                  const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
-              width: screenSize.width,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const LoginScreen()),
-                  );
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
+            SignUpUserButton(screenSize: screenSize),
+            ConfirmUserButton(screenSize: screenSize),
+            LoginUserButton(screenSize: screenSize),
           ],
         ),
       ),
