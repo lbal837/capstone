b78b8a4e1b78abee19333524be566ac587bc39b8
HaoChen <lihaochen987@gmail.com>
2023-04-19 18:48:40 +1200

    Refactor out Signup, Confirm and Login

---
+++ frontend/lib/ui/home/widgets/home_sign_up_user_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/ui/signup/signup_screen.dart';
+
+class SignUpUserButton extends StatelessWidget {
+  const SignUpUserButton({
+    super.key,
+    required this.screenSize,
+  });
+
+  final Size screenSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+      width: screenSize.width,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const SignUpScreen()),
+          );
+        },
+        child: const Text(
+          'Sign Up',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
