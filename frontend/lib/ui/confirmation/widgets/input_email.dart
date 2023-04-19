a424d3c4f33b534ad2d64f918dd505ba936e418f
HaoChen <lihaochen987@gmail.com>
2023-04-19 12:02:29 +1200

    Add InputEmail widget

---
+++ frontend/lib/ui/confirmation/widgets/input_email.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/domain/user.dart';
+import 'package:frontend/ui/confirmation/confirmation_screen.dart';
+
+class InputEmail extends StatelessWidget {
+  const InputEmail({
+    super.key,
+    required this.widget,
+    required User user,
+  }) : _user = user;
+
+  final ConfirmationScreen widget;
+  final User _user;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.email),
+      title: TextFormField(
+        initialValue: widget.email,
+        decoration: const InputDecoration(
+            hintText: 'example@inspire.my', labelText: 'Email'),
+        keyboardType: TextInputType.emailAddress,
+        onSaved: (n) => _user.email = n ?? '',
+      ),
+    );
+  }
+}
