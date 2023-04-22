e29d34934ba8577c741200f13cc85783655cf199
HaoChen <lihaochen987@gmail.com>
2023-04-23 08:44:17 +1200

    Hide `Login` if user is already logged in

--- frontend/lib/ui/home/home.dart
+++ frontend/lib/ui/home/home.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/auth/user_service.dart';
+import 'package:frontend/secrets.dart';
 import 'package:frontend/ui/home/widgets/home_confirm_user_button.dart';
 import 'package:frontend/ui/home/widgets/home_login_user_button.dart';
 import 'package:frontend/ui/home/widgets/home_profile_box.dart';
 import 'package:frontend/ui/home/widgets/home_sign_up_user_button.dart';
 
 class _MyHomePageState extends State<MyHomePage> {
+  final userService = UserService(userPool);
+
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
@@ -13,15 +17,25 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const ProfileBox(),
-            const ProfileBox(),
-            SignUpUserButton(screenSize: screenSize),
-            ConfirmUserButton(screenSize: screenSize),
-            LoginUserButton(screenSize: screenSize),
-          ],
+        child: FutureBuilder<bool>(
+          future: userService.isLoggedIn(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            final bool isLoggedIn = snapshot.data ?? false;
+            debugPrint(isLoggedIn.toString());
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const ProfileBox(),
+                const ProfileBox(),
+                SignUpUserButton(screenSize: screenSize),
+                ConfirmUserButton(screenSize: screenSize),
+                if (!isLoggedIn) LoginUserButton(screenSize: screenSize),
+              ],
+            );
+          },
         ),
       ),
     );
