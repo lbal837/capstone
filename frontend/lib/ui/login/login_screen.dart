2fc37afc05b7668f45b0511946e4494a417ef3aa
HaoChen <lihaochen987@gmail.com>
2023-04-22 15:37:06 +1200

    Rename LoginSubmitButton to LoginUserButton

--- frontend/lib/ui/login/login_screen.dart
+++ frontend/lib/ui/login/login_screen.dart
@@ -115,7 +115,7 @@ class LoginScreenState extends State<LoginScreen> {
                       children: <Widget>[
                         LoginUserEmail(widget: widget, user: _user),
                         LoginUserPassword(user: _user),
-                        LoginSubmitButton(onPressed: () => submit(context)),
+                        LoginUserButton(onPressed: () => submit(context)),
                       ],
                     ),
                   );
