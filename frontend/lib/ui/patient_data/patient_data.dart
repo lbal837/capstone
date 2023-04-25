b86b28bafac4c8801bfe3469e42ea497e9426b56
lbal837 <91184091+lbal837@users.noreply.github.com>
2023-04-26 11:09:08 +1200

    Add profile header widget

--- frontend/lib/ui/patient_data/patient_data.dart
+++ frontend/lib/ui/patient_data/patient_data.dart
@@ -3,6 +3,7 @@ import 'package:frontend/data/patient_repository.dart';
 import 'package:frontend/domain/patient.dart';
 import 'package:frontend/ui/patient_data/widgets/heart_rate_profile_header.dart';
 import 'package:frontend/ui/patient_data/widgets/patient_data_location.dart';
+import 'package:frontend/ui/patient_data/widgets/patient_data_profile_header.dart';
 import 'package:frontend/ui/patient_data/widgets/patient_data_sleep.dart';
 import 'package:frontend/ui/patient_data/widgets/patient_data_step_count.dart';
 
@@ -40,7 +41,7 @@ class _PatientInfoPageState extends State<PatientPage> {
             //final patient_name = patient.fullName,
             ListView(
           children: <Widget>[
-            //ProfileHeader(name: patient?.fullName),
+            ProfileHeader(name: patient?.fullName),
             HeartRateWidget(heartRate: patient?.heartRate.toString()),
             GPSWidget(
                 latitude: patient?.latitude, longitude: patient?.longitude),
