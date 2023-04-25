cc08711f3e71e95a5ccfebcc68f6df2295b0af59
lbal837 <91184091+lbal837@users.noreply.github.com>
2023-04-25 18:18:49 +1200

    able to get data now!

---
+++ Frontend/lib/ui/patient_data/widgets/patient_data_sleep.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SleepWidget extends StatelessWidget {
+  const SleepWidget({Key? key, required String? this.sleepStatus})
+      : super(key: key);
+  final sleepStatus;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: [
+          Container(
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 109, 0, 243),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: const Icon(Icons.bed, size: 70)),
+          Container(
+            padding: const EdgeInsets.all(5.0),
+            alignment: Alignment.center,
+            child: const Text('SleepStatus:'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child: Text(sleepStatus),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ Frontend/lib/ui/patient_data/widgets/patient_data_step_count.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class StepCountWidget extends StatelessWidget {
+  const StepCountWidget({Key? key, required double? this.stepCount})
+      : super(key: key);
+  final stepCount;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: [
+          Container(
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 166, 0, 243),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: const Icon(Icons.directions_walk, size: 70)),
+          Container(
+            padding: const EdgeInsets.all(5.0),
+            alignment: Alignment.center,
+            child: const Text('Todays step count:'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10.0),
+            alignment: Alignment.center,
+            child:
+                Text(stepCount.toString()), //kinda want to make it an int first
+          ),
+        ],
+      ),
+    );
+  }
+}
