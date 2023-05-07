883faa22a8d6013ce845e95901633ea73fb439c8
HaoChen <lihaochen987@gmail.com>
2023-05-08 09:38:22 +1200

    Functionally separated home and login screen

---
+++ frontend/lib/ui/home/widgets/home_auth_buttons_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/ui/home/widgets/home_confirm_user_button.dart';
+import 'package:frontend/ui/home/widgets/home_login_user_button.dart';
+import 'package:frontend/ui/home/widgets/home_sign_up_user_button.dart';
+
+class AuthButtonsScreen extends StatelessWidget {
+  final Size screenSize;
+
+  const AuthButtonsScreen({super.key, required this.screenSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SignUpUserButton(screenSize: screenSize),
+        ConfirmUserButton(screenSize: screenSize),
+        LoginUserButton(screenSize: screenSize),
+      ],
+    );
+  }
+}
