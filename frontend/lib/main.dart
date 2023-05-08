37398babb316cafb3f7e5c311cf4a241321e81e0
HaoChen <lihaochen987@gmail.com>
2023-05-09 08:37:14 +1200

    Initialise Firebase

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/ui/home/home_screen.dart';
 
-void main() {
-  WidgetsFlutterBinding
-      .ensureInitialized(); //i had a binding error so i added this
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
