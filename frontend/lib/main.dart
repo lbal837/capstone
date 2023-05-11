9738ca6b089a07b1fd6438b23e00db42d2a580b9
HaoChen <lihaochen987@gmail.com>
2023-05-11 12:54:02 +1200

    get FCM token from Firebase

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/ui/home/home_screen.dart';
 import 'firebase_options.dart';
@@ -11,8 +12,24 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _getFCMToken();
+  }
+
+  Future<void> _getFCMToken() async {
+    final fcmToken = await FirebaseMessaging.instance.getToken();
+    debugPrint('FCM Token: $fcmToken');
+  }
 
   // This widget is the root of your application.
   @override
