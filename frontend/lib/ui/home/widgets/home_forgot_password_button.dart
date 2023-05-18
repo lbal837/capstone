049ecbcc40d107dbfc6a820caa5c80a9dd09354e
HaoChen <lihaochen987@gmail.com>
2023-05-19 10:10:49 +1200

    Initialised ForgotPassword widgets and screens

---
+++ frontend/lib/ui/home/widgets/home_forgot_password_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/ui/forgot_password/forgot_password_screen.dart';
+
+class ForgotPasswordButton extends StatelessWidget {
+  const ForgotPasswordButton({
+    super.key,
+    required this.screenSize,
+  });
+
+  final Size screenSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 20.0),
+      width: screenSize.width,
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const ForgotPasswordScreen()),
+          );
+        },
+        child: const Text(
+          'Forgot Password',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
