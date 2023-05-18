6a562a95a828df795d5d3c35b0a74d25bade607d
HaoChen <lihaochen987@gmail.com>
2023-05-19 09:37:44 +1200

    Re-added tests

---
+++ frontend/test/home_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Passing test', (WidgetTester tester) async {
+    const pass = true;
+    expect(pass, true);
+  });
+}
