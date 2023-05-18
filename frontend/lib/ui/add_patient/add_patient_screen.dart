e90a709276dc937b055fd8064e5328641befd32c
HaoChen <lihaochen987@gmail.com>
2023-05-18 17:30:10 +1200

    Refactor _subscribeToPatient into userRepository

--- frontend/lib/ui/add_patient/add_patient_screen.dart
+++ frontend/lib/ui/add_patient/add_patient_screen.dart
@@ -1,9 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:frontend/auth/user_service.dart';
+import 'package:frontend/data/user_repository.dart';
 import 'package:frontend/secrets.dart';
-import 'package:http/http.dart' as http;
 
 class AddPatientScreen extends StatefulWidget {
   const AddPatientScreen({Key? key}) : super(key: key);
@@ -15,37 +13,14 @@ class AddPatientScreen extends StatefulWidget {
 class AddPatientScreenState extends State<AddPatientScreen> {
   final TextEditingController _patientIdController = TextEditingController();
   final UserService _userService = UserService(userPool);
+  final UserRepository userRepository = UserDefaultRepository();
 
   Future<void> _subscribeToPatient() async {
     final caregiver = await _userService.getCurrentUser();
     final patientId = _patientIdController.text;
 
     final caregiverEmail = caregiver?.email;
-
-    const apiUrl = '$apiEndpoint/SubscribeCaregiverToPatient';
-
-    try {
-      final response = await http.post(
-        Uri.parse(apiUrl),
-        body: jsonEncode({
-          'caregiver_email': caregiverEmail,
-          'patient_id': patientId,
-        }),
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': apiKey,
-        },
-      );
-
-      if (response.statusCode == 200) {
-        debugPrint('Subscribed to patient with ID: $patientId');
-      } else {
-        debugPrint(
-            'Failed to subscribe to patient. Status code: ${response.statusCode}');
-      }
-    } catch (e) {
-      debugPrint('Failed to subscribe to patient: $e');
-    }
+    await userRepository.subscribeToPatient(caregiverEmail!, patientId);
   }
 
   @override
