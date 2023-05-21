9059e04fe460faf3a734648aa852bd166d0e930c
HaoChen <lihaochen987@gmail.com>
2023-05-21 14:41:14 +1200

    Add backup of AddUserToPatientv2

---
+++ backend/AddUserToPatientv2/lambda_function.py
@@ -0,0 +1,61 @@
+import os
+import psycopg2
+import sys
+import json
+
+sys.path.insert(0, "/var/task/dependencies")
+
+def add_patient_to_user(user_id, patient_id):
+    db_host = os.environ.get("DB_HOST")
+    db_user = os.environ.get("DB_USER")
+    db_password = os.environ.get("DB_PASSWORD")
+    db_name = os.environ.get("DB_NAME")
+
+    conn = psycopg2.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        dbname=db_name
+    )
+
+    with conn:
+        with conn.cursor() as cur:
+            # Get the current patient_ids for the user
+            cur.execute("SELECT patient_ids FROM users WHERE email = %s", (user_id,))
+            result = cur.fetchone()
+            if result:
+                patient_ids = result[0] or []
+            else:
+                return {"error": "User not found"}
+
+            # Add the new patient_id to the patient_ids list, if it doesn't already exist
+            if patient_id not in patient_ids:
+                patient_ids.append(patient_id)
+
+                # Update the patient_ids in the users table
+                cur.execute(
+                    "UPDATE users SET patient_ids = %s WHERE email = %s",
+                    (patient_ids, user_id)
+                )
+
+    conn.close()
+
+# Lambda function
+def lambda_handler(event, context):
+    # Debugging.
+    print(event)
+    print("********")
+    print(context)
+    print("********")
+
+    body = json.loads(event["body"])
+    user_id = body["UserId"]
+    patient_id = body["PatientId"]
+    
+    try:
+        result = add_patient_to_user(user_id, patient_id)
+        if "error" in result:
+            return {"status": "error", "message": result["error"]}
+        return {"status": "success"}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
