c608e96d6f7d2c771288d9d49659ef57530303d6
HaoChen <lihaochen987@gmail.com>
2023-05-22 11:42:04 +1200

    Refactor out add_patient_button.dart

--- frontend/lib/ui/add_patient/add_patient_screen.dart
+++ frontend/lib/ui/add_patient/add_patient_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:frontend/auth/user_service.dart';
 import 'package:frontend/data/user_repository.dart';
 import 'package:frontend/secrets.dart';
+import 'package:frontend/ui/add_patient/widget/add_patient_button.dart';
 import 'package:frontend/ui/add_patient/widget/add_patient_id.dart';
 
 class AddPatientScreen extends StatefulWidget {
@@ -46,25 +47,9 @@ class AddPatientScreenState extends State<AddPatientScreen> {
           children: [
             AddPatientId(controller: _patientIdController),
             const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                final completer = Completer<void>();
-                try {
-                  _subscribeToPatient()
-                      .then((_) => _addPatientToUser())
-                      .then((_) {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text(
-                            'Patient successfully added and subscribed!')));
-                    completer.complete();
-                  });
-                } catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text('Failed to add or subscribe patient!')));
-                  completer.completeError(e);
-                }
-              },
-              child: const Text('Add Patient'),
+            AddPatientButton(
+              subscribeToPatient: _subscribeToPatient,
+              addPatientToUser: _addPatientToUser,
             ),
           ],
         ),
