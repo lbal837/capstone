6f61555ce7e75ef2264772a2aa6da7100113e80e
HaoChen <lihaochen987@gmail.com>
2023-05-22 09:55:33 +1200

    Refactored out confirm_reset_password_confirmation_code.dart widget

---
+++ frontend/lib/ui/confirm_reset_password/widgets/confirm_reset_password_confirmation_code.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ConfirmResetPasswordCode extends StatelessWidget {
+  final TextEditingController controller;
+
+  const ConfirmResetPasswordCode({Key? key, required this.controller})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.confirmation_num),
+      title: TextFormField(
+        controller: controller,
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter the confirmation code';
+          }
+          return null;
+        },
+        decoration: const InputDecoration(
+          labelText: 'Confirmation Code',
+        ),
+      ),
+    );
+  }
+}
