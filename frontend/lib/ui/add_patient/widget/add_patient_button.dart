87d6fdab2010783776d092288a6d07ef465d0a5b
HaoChen <lihaochen987@gmail.com>
2023-05-22 11:43:30 +1200

    Change add_patient_button.dart size

--- frontend/lib/ui/add_patient/widget/add_patient_button.dart
+++ frontend/lib/ui/add_patient/widget/add_patient_button.dart
@@ -14,22 +14,30 @@ class AddPatientButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        final completer = Completer<void>();
-        try {
-          subscribeToPatient().then((_) => addPatientToUser()).then((_) {
+    final screenSize = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.all(20.0),
+      width: screenSize.width,
+      margin: const EdgeInsets.only(
+        top: 10.0,
+      ),
+      child: ElevatedButton(
+        onPressed: () {
+          final completer = Completer<void>();
+          try {
+            subscribeToPatient().then((_) => addPatientToUser()).then((_) {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text('Patient successfully added and subscribed!')));
+              completer.complete();
+            });
+          } catch (e) {
             ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                content: Text('Patient successfully added and subscribed!')));
-            completer.complete();
-          });
-        } catch (e) {
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text('Failed to add or subscribe patient!')));
-          completer.completeError(e);
-        }
-      },
-      child: const Text('Add Patient'),
+                content: Text('Failed to add or subscribe patient!')));
+            completer.completeError(e);
+          }
+        },
+        child: const Text('Add Patient'),
+      ),
     );
   }
 }
