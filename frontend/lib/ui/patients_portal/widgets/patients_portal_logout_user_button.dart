814e2b397c895e9aaca08f740b070a67133c79fc
HaoChen <lihaochen987@gmail.com>
2023-05-22 16:06:00 +1200

    Remove unused import

--- frontend/lib/ui/patients_portal/widgets/patients_portal_logout_user_button.dart
+++ frontend/lib/ui/patients_portal/widgets/patients_portal_logout_user_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/auth/user_service.dart';
-import 'package:frontend/ui/home/home_screen.dart';
 
 class PatientsPortalLogoutUserButton extends StatelessWidget {
   final UserService userService;
