1ab43b98aac993b495615f6d5f874e0eefbc6c87
HaoChen <lihaochen987@gmail.com>
2023-05-22 16:23:38 +1200

    Remove unused imports

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:frontend/ui/home/home_screen.dart';
 import 'color_schemes.g.dart';
 import 'firebase_options.dart';
 
--- frontend/lib/ui/initiate_reset_password/initiate_reset_password_screen.dart
+++ frontend/lib/ui/initiate_reset_password/initiate_reset_password_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/auth/user_service.dart';
 import 'package:frontend/secrets.dart';
-import 'package:frontend/ui/confirm_reset_password/confirm_reset_password_screen.dart';
 import 'package:frontend/ui/initiate_reset_password/widgets/initiate_reset_password_button.dart';
 import 'package:frontend/ui/initiate_reset_password/widgets/initiate_reset_password_email.dart';
 
