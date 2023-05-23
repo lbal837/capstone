60e266fe1ac3e9ff1ccd49b9d04d71a94e3aed6a
HaoChen <lihaochen987@gmail.com>
2023-05-23 19:24:03 +1200

    Did some stuff ig???

--- frontend/lib/ui/patient_data/widgets/patient_data_profile_header.dart
+++ frontend/lib/ui/patient_data/widgets/patient_data_profile_header.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
 
-
 class ProfileHeader extends StatelessWidget {
-
-
-  const ProfileHeader({Key? key, required this.name, required this.picture,}) : super(key: key);
+  const ProfileHeader({
+    Key? key,
+    required this.name,
+    required this.picture,
+    required this.isConnected,
+  }) : super(key: key);
   final String? name;
   final String? picture;
+  final bool isConnected;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Container(
-            padding: const EdgeInsets.all(30.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
+          padding: const EdgeInsets.all(30.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: CircleAvatar(
+            radius: 70, // Image radius
+            backgroundImage: NetworkImage(
+              picture!,
             ),
-            child: CircleAvatar(
-  radius: 70, // Image radius
-  backgroundImage: NetworkImage(picture!, 
-                              ),
-),),
+          ),
+        ),
         Container(
           padding: const EdgeInsets.all(5.0),
           alignment: Alignment.center,
