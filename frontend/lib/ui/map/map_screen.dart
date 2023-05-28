144ad1a7d0c33eebea0d721b4f31e52616dc10c7
HaoChen <lihaochen987@gmail.com>
2023-05-28 15:45:28 +1200

    Add coordinates to map_screen.dart

--- frontend/lib/ui/map/map_screen.dart
+++ frontend/lib/ui/map/map_screen.dart
@@ -15,32 +15,49 @@ class MapScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Map'),
       ),
-      body: FlutterMap(
-        options: MapOptions(
-          center: LatLng(latitude, longitude),
-          zoom: 15.0,
-        ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://api.mapbox.com/styles/v1/'
-                '{id}/tiles/{z}/{x}/{y}@2x?access_token={accessToken}',
-            additionalOptions: const {
-              'accessToken': mapboxSecretAccessToken,
-              'id': 'mapbox/streets-v11',
-            },
-          ),
-          MarkerLayer(
-            markers: [
-              Marker(
-                width: 40.0,
-                height: 40.0,
-                point: LatLng(latitude, longitude),
-                builder: (ctx) => const Icon(
-                  Icons.location_pin,
-                  color: Colors.red,
-                ),
+      body: Stack(
+        children: <Widget>[
+          FlutterMap(
+            options: MapOptions(
+              center: LatLng(latitude, longitude),
+              minZoom: 15.0,
+              maxZoom: 18.0,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://api.mapbox.com/styles/v1/'
+                    '{id}/tiles/{z}/{x}/{y}@2x?access_token={accessToken}',
+                additionalOptions: const {
+                  'accessToken': mapboxSecretAccessToken,
+                  'id': 'mapbox/streets-v11',
+                },
+              ),
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    width: 40.0,
+                    height: 40.0,
+                    point: LatLng(latitude, longitude),
+                    builder: (ctx) => const Icon(
+                      Icons.location_pin,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
               ),
             ],
+          ),
+          Positioned(
+            top: 10.0,
+            left: 10.0,
+            child: Text(
+              'Latitude: $latitude, Longitude: $longitude',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                backgroundColor: Colors.black.withOpacity(0.7),
+              ),
+            ),
           ),
         ],
       ),
