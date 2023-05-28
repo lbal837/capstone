3b4c22017fad05ab2a16556f7327fcf585f3e1b0
HaoChen <lihaochen987@gmail.com>
2023-05-28 17:24:24 +1200

    Add settings_screen.dart and moved settings_logout_user_button.dart

---
+++ frontend/lib/ui/settings/settings_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/auth/user_service.dart';
+import 'package:frontend/ui/settings/widgets/settings_logout_user_button.dart';
+
+class SettingsScreen extends StatelessWidget {
+  final UserService userService;
+
+  const SettingsScreen({Key? key, required this.userService}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: Column(
+        children: <Widget>[
+          SettingsLogoutUserButton(
+              userService: userService, screenSize: screenSize),
+        ],
+      ),
+    );
+  }
+}
