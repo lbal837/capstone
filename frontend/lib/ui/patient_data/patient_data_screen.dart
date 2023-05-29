3064b5ed85d08bae4560b73297fd7467fdb7a460
HaoChen <lihaochen987@gmail.com>
2023-05-30 11:40:43 +1200

    Implement implicit delete

--- frontend/lib/ui/patient_data/patient_data_screen.dart
+++ frontend/lib/ui/patient_data/patient_data_screen.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/auth/user_service.dart';
 import 'package:frontend/data/user_repository.dart';
 import 'package:frontend/domain/patient.dart';
+import 'package:frontend/domain/response.dart';
+import 'package:frontend/secrets.dart';
 import 'package:frontend/ui/patient_data/widgets/heart_rate_profile_header.dart';
 import 'package:frontend/ui/patient_data/widgets/patient_data_location.dart';
 import 'package:frontend/ui/patient_data/widgets/patient_data_profile_header.dart';
 import 'package:frontend/ui/patient_data/widgets/patient_data_sleep.dart';
 import 'package:frontend/ui/patient_data/widgets/patient_data_step_count.dart';
+import 'package:frontend/ui/remove_patient/widget/remove_patient_button.dart';
 import 'package:intl/intl.dart';
 
 class PatientDataScreenState extends State<PatientDataScreen> {
   Patient? patient;
   bool isLoaded = false;
+  final UserService _userService = UserService(userPool);
+  final UserRepository userRepository = UserDefaultRepository();
 
   @override
   void initState() {
@@ -20,12 +26,28 @@ class PatientDataScreenState extends State<PatientDataScreen> {
 
   Future<void> getData() async {
     final UserRepository userRepository = UserDefaultRepository();
-    patient = await userRepository.fetchPatient(widget.userId);
+    patient = await userRepository.fetchPatient(widget.patientId);
     if (patient != null) {
       setState(() {
         isLoaded = true;
       });
     }
+  }
+
+  Future<Response> _unsubscribeFromPatient() async {
+    final caregiver = await _userService.getCurrentUser();
+
+    final caregiverEmail = caregiver?.email;
+    return userRepository.unsubscribeFromPatient(
+        caregiverEmail!, patient!.userId);
+  }
+
+  Future<Response> _removePatientFromUser() async {
+    final caregiver = await _userService.getCurrentUser();
+
+    final caregiverEmail = caregiver?.email;
+    return userRepository.removePatientFromUser(
+        caregiverEmail!, patient!.userId);
   }
 
   bool isWithinOneMinute(String dateTimeString) {
@@ -57,6 +79,9 @@ class PatientDataScreenState extends State<PatientDataScreen> {
               picture: patient?.avatarImage,
               isConnected: isWithinOneMinute(patient!.dateTime),
             ),
+            RemovePatientButton(
+                unsubscribeFromPatient: _unsubscribeFromPatient,
+                removePatientFromUser: _removePatientFromUser),
             HeartRateWidget(heartRate: patient?.heartRate.toString()),
             GPSWidget(
               latitude: patient?.latitude,
@@ -81,9 +106,9 @@ class PatientDataScreenState extends State<PatientDataScreen> {
 }
 
 class PatientDataScreen extends StatefulWidget {
-  const PatientDataScreen({super.key, required this.userId});
+  const PatientDataScreen({super.key, required this.patientId});
 
-  final String userId;
+  final String patientId;
 
   @override
   State<PatientDataScreen> createState() => PatientDataScreenState();
